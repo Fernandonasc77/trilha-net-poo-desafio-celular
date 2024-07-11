6d10eb8a1dbb8d9d9ea318dfe2baec86cc8ebd2b
Fernando Nascimento <fernando7nasc@gmail.com>
2024-07-11 20:42:47 -0300

    Ultimas modificações para conclusão

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+        
+static void Main(string[] args)
+        
+    {
+        Console.WriteLine("Smartphone Nokia");
+        Smartphone nokia = new Nokia(numero: "1234560", modelo: "0104", imei: "11111111", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("whatssapp");
+
+
+        Console.WriteLine("\n");
+
+
+        Console.WriteLine("Smartphone Iphone");
+        Smartphone iphone = new Iphone(numero: "326565", modelo: "2240", imei: "22222222", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+
+}
